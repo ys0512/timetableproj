7f3b0683a5c0c40a816165af8a5d361799c4c6d5
ys0512 <y.sasaki.0512.pc@gmail.com>
2023-05-17 23:10:01 +0900

    Update basic.cy.ts

--- cypress/e2e/basic.cy.ts
+++ cypress/e2e/basic.cy.ts
@@ -5,7 +5,7 @@ describe('empty spec', () => {
 
   it('display header text', () => {
     cy.get('h1')
-    .contains('Hello World!', { matchCase: false })
+    .contains('Hello World!2', { matchCase: false })
   })
 
   it('displays Click button', () => {
