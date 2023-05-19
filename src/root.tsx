1f9d06228eb1f7dc30e7b20f19bc85914d469e02
ys0512 <y.sasaki.0512.pc@gmail.com>
2023-05-19 19:12:32 +0900

    Update root.tsx

--- src/root.tsx
+++ src/root.tsx
@@ -26,7 +26,7 @@ export default function Root() {
         <Suspense>
           <ErrorBoundary>
             <A href="/">Index</A>
-            <A href="/about">About</A>
+            <A href="/about">About2</A>
             <Routes>
               <FileRoutes />
             </Routes>
