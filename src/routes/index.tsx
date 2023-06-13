d3f8f38257029c7c7e586493d735d477dc297afe
ys0512 <y.sasaki.0512.pc@gmail.com>
2023-06-13 22:44:11 +0900

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,6 +7,12 @@ export default function Home() {
       <Title>Hello World</Title>
       <h1>Hello world!_!</h1>
       <Counter />
+      
+      test
+      <a href="comgooglemaps://">Google Mapsのアプリを開く</a>
+      <a href="twitter://user?screen_name=digital_jpn">Twitterアカウント</a>
+      <a href="twitter://">Twitterだけ</a>
+      
       <p>
         Visit{" "}
         <a href="https://start.solidjs.com" target="_blank">
