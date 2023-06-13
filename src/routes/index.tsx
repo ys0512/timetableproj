6cafee861e693a167b1976da7d0ac905b928f434
ys0512 <y.sasaki.0512.pc@gmail.com>
2023-06-13 22:19:50 +0900

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main>
       <Title>Hello World</Title>
-      <h1>Hello world!</h1>
+      <h1>Hello world!_!</h1>
       <Counter />
       <p>
         Visit{" "}
