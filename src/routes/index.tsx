e5dd2d051b949371f20c1c11dfdb06bc316e28d5
ys0512 <y.sasaki.0512.pc@gmail.com>
2023-06-18 15:37:33 +0900

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,23 +4,9 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!_!9</h1>
-      <Counter />
-      
-      test
-      <a href="comgooglemaps://">Google Mapsのアプリを開く</a>
-      <a href="twitter://user?screen_name=digital_jpn">Twitterアカウント</a>
-      <a href="twitter://">Twitterだけ</a>
-      <a href="https://twitter.com/digital_jpn" target="_blank">デジタル庁</a>
-      
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+    <div>
+      <iframe src="../../index.html" />
+    </div>
     </main>
   );
 }
